fc78b8a0d075815711c37301cb4c321e3ffa37d7
icy-s <evgeny.tailov@gmail.com>
2025-01-17 14:51:33 +0200

    )

--- kino_work03/User_choose.Designer.cs
+++ kino_work03/User_choose.Designer.cs
@@ -31,6 +31,10 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -56,17 +60,54 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(416, 58);
+            this.pictureBox1.Location = new System.Drawing.Point(411, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(284, 292);
+            this.pictureBox1.Size = new System.Drawing.Size(350, 426);
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(275, 154);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(109, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(275, 225);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(109, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(213, 157);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Film Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(213, 228);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Film Year";
             // 
             // User_choose
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.button2);
@@ -74,6 +115,7 @@
             this.Text = "User_choose";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -82,5 +124,9 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- kino_work03/User_choose.cs
+++ kino_work03/User_choose.cs
@@ -1,38 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
 using System.IO;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-    namespace kino_work03
+namespace kino_work03
+{
+    public partial class User_choose : Form
     {
-        public partial class User_choose : Form
+        public User_choose()
         {
-            public User_choose()
-            {
-                InitializeComponent();
-            }
+            InitializeComponent();
+        }
 
-        string path = @"C:\Users\opilane.TTHK\source\repos\kino_work03\kino_work03\images.txt";
         int tt = 0;
-        List<string> pildid = new List<string>();   
+        List<string> pildid = new List<string>();
+        List<string> names = new List<string>();
+        List<string> years = new List<string>();
 
         private void button1_Click(object sender, EventArgs e)
         {
             string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=kino;Integrated Security=True";
-            string query = "SELECT filmImg FROM film";
+            string query = "SELECT filmImg FROM film"; // Предполагаем, что в базе данных хранится URL изображения
+            string queryName = "SELECT filmName FROM film";
+            string queryYear = "SELECT filmYear FROM film";
 
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 try
                 {
+                    pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
                     connection.Open();
                     using (SqlCommand command = new SqlCommand(query, connection))
                     {
@@ -40,16 +39,50 @@ using System.Windows.Forms;
                         {
                             while (reader.Read())
                             {
-                                string imagePath = reader["filmImg"].ToString();
-                                pildid.Add(imagePath);
+                                string imageUrl = reader["filmImg"].ToString(); // Предполагаем, что filmImg это URL
+                                pildid.Add(imageUrl);
+                            }
+                        }
+                    }
+
+                    using (SqlCommand command = new SqlCommand(queryName, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                string name = reader["filmName"].ToString(); // Предполагаем, что filmImg это URL
+                                names.Add(name);
+                            }
+                        }
+                    }
+
+                    using (SqlCommand command = new SqlCommand(queryYear, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                string year = reader["filmYear"].ToString(); // Предполагаем, что filmImg это URL
+                                years.Add(year);
                             }
                         }
                     }
 
                     if (pildid.Count > 0)
                     {
-                        string fail = pildid[tt];
-                        pictureBox1.Image = Image.FromFile(fail);
+                        string imageUrl = pildid[tt];
+
+                        string name = names[tt];
+
+                        string year = years[tt];
+
+                        textBox1.Text = name;
+
+                        textBox2.Text = year;
+
+                        pictureBox1.Image = LoadImageFromUrl(imageUrl);
+
                         tt++;
 
                         if (tt == pildid.Count)
@@ -69,12 +102,36 @@ using System.Windows.Forms;
             }
         }
 
-        private void button2_MouseClick(object sender, MouseEventArgs e)
+        private Image LoadImageFromUrl(string imageUrl)
+        {
+            try
             {
-                this.Hide();
-                User_buy_tickets User_buy_tickets = new User_buy_tickets();
-                User_buy_tickets.Show();
+                using (WebClient webClient = new WebClient())
+                {
+                    byte[] imageBytes = webClient.DownloadData(imageUrl);
+                    using (MemoryStream ms = new MemoryStream(imageBytes))
+                    {
+                        return Image.FromStream(ms);
+                    }
+                }
             }
-        }       
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при загрузке изображения: " + ex.Message);
+                return null;
+            }
+        }
+
+        private void button2_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.Hide();
+            User_buy_tickets User_buy_tickets = new User_buy_tickets();
+            User_buy_tickets.Show();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
-        
+}
